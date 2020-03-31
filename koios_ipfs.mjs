3d9ecaa3c37cd4dcd6660551748f8cda4ec7a71d
CryingEagle1 <31490364+CryingEagle1@users.noreply.github.com>
2020-04-01 00:18:41 +0200

    from pc

--- koios_ipfs.mjs
+++ koios_ipfs.mjs
@@ -1,7 +1,5 @@
-/*jshint esversion: 8, maxstatements:15, maxparams:3, maxdepth:3, maxcomplexity:5*/
-
-console.log(`In ${window.location.href} starting script: ${import.meta.url}`);
-import {} from './koios_youtube.mjs';
+//console.log(`In ${window.location.href} starting script: ${import.meta.url}`);
+import {GetYouTubePlaylists as getYouTubePlaylists, GetYouTubePlayListItems as getYouTubePlayListVideos} from './koios_youtube.mjs';
 
 
 export class Ipfs
