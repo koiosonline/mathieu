2b0fc68da09ea88130333b929664d93e873b5b64
CryingEagle1 <31490364+CryingEagle1@users.noreply.github.com>
2020-04-06 01:04:46 +0200

    upload and get ipfs info

--- koios_ipfs.mjs
+++ koios_ipfs.mjs
@@ -17,66 +17,22 @@ export async function uploadToIpfs()
 {
   var ipfs = await setupIPFS();
   var hash;                   //IPFS hash
-  /*
-  ipfs.add(forIPFSexport()).then((response) => {
-    console.log(response);
-    // CID of wrapping directory is returned last
-    hash = response[response.length - 1].hash;
-    console.log("upload-info-fromjs", hash);
-    console.log(hash);
-  }).catch((err) => {
-    console.error(err);
-  });
-*/
   for await (const result of ipfs.add(JSON.stringify(await forIPFSexport())))
   {
     console.log(result);
+    hash = result.path;
   }
-
+  return hash;
 }
 
 export async function getInfoIpfs(hash)
 {
   var ipfs = await setupIPFS();
-
-}
-
-/*
-uploadForm.addEventListener('submit', e => {      //create an EventListener to react on the submit button press
-  e.preventDefault();                       //prevent the default action from firing
-
-  //IPFS.add requires the files to be an object in array format
-  var fileArr = [];
-  var hash;                                 //IPFS Hash
-  const files = document.querySelector('[type=file]').files;
-  //querySelector selects first documentElement with type file. Selected files are now addressable.
-
-  for (let i = 0; i < files.length; i++) {  //Puts files in the array and right format
-    var file = files[i];
-    var fileObj = {
-      path: file.name,
-      content: file
-    };
-    fileArr[i] = fileObj;
+  var videoAndPlaylistInfo;
+  for await (const result of ipfs.cat(hash))
+  {
+    //console.log(JSON.parse(result.toString('utf8')));
+    videoAndPlaylistInfo = JSON.parse(result.toString('utf8'));
   }
-
-  console.log(fileArr);
-
-  //Options to wrap files in ipfs directory
-  const options = {
-    wrapWithDirectory: true,
-    progress: (prog) => console.log(`received: ${prog}`)
-  };
-
-  ipfs.add(fileArr, options).then((response) => {
-    console.log(response);
-    // CID of wrapping directory is returned last
-    hash = response[response.length - 1].hash;
-    log("upload-info-fromjs", hash);
-    console.log(hash);
-  }).catch((err) => {
-    console.error(err);
-  });
-  //saveToIpfsWithFilename(files);
-});
-*/
+  return videoAndPlaylistInfo;
+}
